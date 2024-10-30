318b35d498b84ec97ad4dfb779b3654bdad4ae0e
ElioEnestrom <elio@enestrom.com>
2024-10-30 12:48:45 +0100

    Basic thing done

--- CrestEngine/CrestEngine/main.cpp
+++ CrestEngine/CrestEngine/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <glad/glad.h>
 #include <glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 int main() 
 {
@@ -10,19 +13,54 @@ int main()
 		return -1;
 	}
 
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 	GLFWwindow* window;
 	window = glfwCreateWindow(640, 480, "Hello, World!", NULL, NULL);
 
-	glfwMakeContextCurrent(window);
-
-	while (true)
+	if (window == NULL)
 	{
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glClearColor(0.4, 0.3, 0.2, 1);
-
-		glfwSwapBuffers(window);
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
 	}
 
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); 
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	//while (true)
+	//{
+	//	glClear(GL_COLOR_BUFFER_BIT);
+	//
+	//	glClearColor(0.4, 0.3, 0.2, 1);
+	//
+	//	glfwSwapBuffers(window);
+	//}
+
+	glfwTerminate();
 	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
 }
